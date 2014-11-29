2694350c5146308a6b3858730bc5d7d6d722ed25
James D. Callahan III <jcallahan@curse.com>
2014-11-29 04:35:56 -0600

    Alphabetized localization entries.

--- Locales/devel-enUS.lua
+++ Locales/devel-enUS.lua
@@ -3,15 +3,15 @@ local L = LibStub("AceLocale-3.0"):NewLocale(FOLDER_NAME, "enUS", true)
 
 if not L then return end
 
+L.ALT_KEY = "Alt-"
+L.COLUMN_LABEL_REALM = "Realm"
+L.CONTROL_KEY = "Control-"
+L.LEFT_CLICK = "Left-Click"
 L.MINIMAP_ICON_DESC = "Show the interface as a minimap icon."
-L.TOOLTIP_HIDEDELAY_LABEL = "Tooltip Hide Delay"
-L.TOOLTIP_HIDEDELAY_DESC = "Length of time the tooltip will continue to be shown after the mouse has been moved."
-L.TOOLTIP_SCALE_LABEL = "Tooltip Scale"
 L.NOTES_ARRANGEMENT_COLUMN = "Column"
 L.NOTES_ARRANGEMENT_ROW = "Row"
-L.COLUMN_LABEL_REALM = "Realm"
 L.RIGHT_CLICK = "Right-Click"
-L.LEFT_CLICK = "Left-Click"
-L.CONTROL_KEY = "Control-"
 L.SHIFT_KEY = "Shift-"
-L.ALT_KEY = "Alt-"
+L.TOOLTIP_HIDEDELAY_DESC = "Length of time the tooltip will continue to be shown after the mouse has been moved."
+L.TOOLTIP_HIDEDELAY_LABEL = "Tooltip Hide Delay"
+L.TOOLTIP_SCALE_LABEL = "Tooltip Scale"
